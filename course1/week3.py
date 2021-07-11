087d8532f44d09677ed9f1e40c299641a205106b
Subhankar Sen <SUBHANKARSEN2001@GMAIL.COM>
2021-07-11 21:35:05 +0530

    added the course1 exercises

--- course1/week3.py
+++ course1/week3.py
@@ -6,7 +6,7 @@ from torchvision.datasets import MNIST
 from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-torch.manual_seed(0) # Set for testing purposes, please do not change!
+torch.manual_seed(0)
 
 def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
     '''
